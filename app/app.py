6b29e60424c94a4e881ba1f4cadbd6b4edec11a4
Mohak kataria <mohak.kumar53@gmail.com>
2026-03-22 16:38:31 +0530

    Added codebase for dashboard

--- app/app.py
+++ app/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def dashboard():
     data = {
-        "pipeline": "SUCCESS ✅",
-        "build": "PASSED ✅",
-        "security": "NO CRITICAL VULNERABILITIES 🔐",
-        "deployment": "RUNNING 🚀"
+        "status": "SUCCESS",
+        "last_build": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "vulnerabilities": "0 CRITICAL",
+        "app_status": "Running"
     }
-    return render_template("index.html", data=data)
+    return render_template("dashboard.html", data=data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
